48c6344f7e8e85015c9a78b029cc59025614a71f
Anna <anna-bogacheva86@yandex.ru>
2022-01-30 15:01:36 +0500

    correction of task 5 ("/" instead of "//" in the last line)

--- task_5/5.py
+++ task_5/5.py
@@ -4,7 +4,7 @@ if proceeds > cost:
     print('Фирма работает с прибылью!')
     print('Рентабельность фирмы равна: ', ((proceeds - cost) / proceeds) * 100, '%')
     worker = int(input('Введите численность сотрудников фирмы: '))
-    print('Прибыль фирмы в расчете на одного сотрудника равна: ', (proceeds - cost) // worker)
+    print('Прибыль фирмы в расчете на одного сотрудника равна: ', (proceeds - cost) / worker)
 else:
     print('Фирма работает в убыток!')
     
